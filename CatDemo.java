e1c097e1ffe87cf07a4a31fd4e6908e51483efa1
rsveinson <rsveinson@mts.net>
2023-11-28 16:12:13 -0600

    added some extra association code Owner now adds a cat's owner when the cat is added to the owner's list

--- CatDemo.java
+++ CatDemo.java
@@ -11,7 +11,7 @@ public class CatDemo{
     private int age;
     private String name;
     private int id;         // this will be a unique id nuber
-    
+
     private Owner owner;
 
     // constructors
@@ -31,50 +31,49 @@ public class CatDemo{
     public int getAge(){
         return age;
     }// end get age
-    
+
     public String getName(){
         return name;
     }// end get name
-    
+
     public int getID(){
         return this.id;
     }// end get id
-    
+
     public Owner getOwner(){
         return this.owner;
     }// end get owner
-    
+
     // setters
     // no setter for id, we don't want to allow any chnages once the unique id has been assigned
     public void setAge(int a){
         age = a;
     }// end setage
-    
+
     public void setName(String n){
         name = n;
     }// end set name
-    
+
     public void setOwner(Owner owner){
         this.owner = owner;
     }// end set owner
-    
+
     // other methods
     // get the next available id and increment nextID
     // so the next cat gets a unique id number
     private int getNextID(){
         return nextID++;
     }// end getNextID
-    
+
     @Override
     public String toString(){
         String st;
-        
+
         st = this.getName() + ", " + this.getID() + ", " + this.getAge();
         st += ": " + this.getOwner();
         return st;
     }// end toString
-    
-    
+
     public boolean equals(CatDemo oc){
         //return this.name.equals(oc.name);
         //return this.age == oc.age;
@@ -82,6 +81,5 @@ public class CatDemo{
         e = this.name.equals(oc.name) && this.age == oc.age;
         return e;
     }// end equals 
-    
-    
+
 }
--- Owner.java
+++ Owner.java
@@ -3,66 +3,64 @@ import java.util.ArrayList;
 /** 
  * ACS-1903 Assignment X Question Y
  * @author 
-*/
+ */
 
 public class Owner{
     // class variables
-    
+
     // instance variables
     private String name;
-    
+
     // for associating with a bunch of cats
     private ArrayList<Cat> myCats;
-    
+
     // constructors
     public Owner(){
         name = "Unknown";
-        
+
         // note that here is where the ArrayList is intantiated
         myCats = new ArrayList<>();
     }// end no-arg
-    
+
     public Owner(String name){
         this.name = name;
         myCats = new ArrayList<>();
     }// end full-arg
-    
+
     // getters
     public String getName(){
         return name;
     }// end get name
-    
-    
+
     public ArrayList<Cat> getCats(){
         return myCats;
     }// end get cats
-    
-    
+
     // setters
     public void setName(String name){
         this.name = name;
     }// end set name
-    
-    
+
     // here we set the entire list of cats
     public void setCats(ArrayList<Cat> l){
         myCats = l;
     }// end set Cats
-    
-    
+
     // perhaps a more useful method to add a single 
     // cat to the existing list of cats
     public void addCat(Cat cat){
         myCats.add(cat);
+        
+        // add this owner to their new cat
+        cat.setOwner(this);
     }// end addCat
-    
-    
+
     // others
     // get an individual cat/
     public Cat getACat(int index){
         return myCats.get(index);
     }// end a specific cat from the list
-    
+
     @Override
     public String toString(){
         String st = name+"'s Cats:\n";
@@ -72,7 +70,5 @@ public class Owner{
         }//end for
         return st;
     }// end to string
-    
-    
-    
-}
+
+    }
